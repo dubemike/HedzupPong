a337a4d52d79382d7bc44ffefac06e4cf3dee2df
dubemike <dubemike@outlook.com>
2014-10-02 22:12:58 +0200

    Initial Commit

---
+++ HedzupPongGame/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  HedzupPongGame
+//
+
+//  Copyright (c) 2014 tappnology. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ HedzupPongGame/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  HedzupPongGame
+//
+//  Created by Michael Dube on 2014/10/02.
+//  Copyright (c) 2014 tappnology. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
